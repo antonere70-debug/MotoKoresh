5667f03af54d99ebca841783af25996d3f0adeb6
antonere70-debug <antonere70@gmail.com>
2026-06-09 16:43:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ BOT_USERNAME = os.getenv("BOT_USERNAME")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 PORT = int(os.getenv("PORT", "10000"))
+
+RESTART_INTERVAL_HOURS = 2
+MEMORY_RETENTION_HOURS = 2
